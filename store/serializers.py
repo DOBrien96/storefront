c57c582c2452bc2f14ce4eb84879865d139f6967
Dylan O'Brien <dylanobrien1985@gmail.com>
2023-06-06 16:11:49 +0100

    Applying basic filtering to the products api

--- store/serializers.py
+++ store/serializers.py
@@ -27,4 +27,6 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = ['id', 'date', 'name', 'description', 'product']
 
-   
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return Review.objects.create(product_id=product_id, **validated_data)

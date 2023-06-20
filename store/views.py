8afed7ba154cd14cee85a351af10c98142296354
Dylan O'Brien <dylanobrien1985@gmail.com>
2023-06-20 20:19:36 +0100

    Create order id & post to database.

--- store/views.py
+++ store/views.py
@@ -111,6 +111,14 @@ class OrderViewSet(ModelViewSet):
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
     
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateOrderSerializer
+        return OrderSerializer
+    
+    def get_serializer_context(self):
+        return {'user_id': self.request.user.id}
+    
     def get_queryset(self):
         user = self.request.user
         

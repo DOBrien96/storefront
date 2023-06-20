0baedf38adf467bb20ee2ba38b46fda944d05ce7
Dylan O'Brien <dylanobrien1985@gmail.com>
2023-06-20 20:52:42 +0100

    Implenting data validation to cart

--- store/serializers.py
+++ store/serializers.py
@@ -127,6 +127,13 @@ class OrderSerializer(serializers.ModelSerializer):
 class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
     
+    def validate_cart_id(self, cart_id):
+        if Cart.objects.filter(pk=cart_id).exists():
+            raise serializers.ValidationError('No cart with the given ID was fount')
+        if CartItem.objects.filter(cart_id=cart_id).count() == 0:
+            raise serializers.ValidationError('This cart is empty.')
+        return cart_id
+        
     def save(self):
         with transaction.atomic():        
             cart_id = self.validated_data['cart_id']

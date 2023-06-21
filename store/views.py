c8664143d2061b119608417f88b5d41d4cb376df
Dylan O'Brien <dylanobrien1985@gmail.com>
2023-06-21 21:43:43 +0100

    Ability to fetch images from the API

--- store/views.py
+++ store/views.py
@@ -16,8 +16,9 @@ from .filters import ProductFilter
 from .pagination import DefaultPagination
 from .permissions import FullDjangoModelPermissions, IsAdminOrReadOnly, ViewCustomerHistoryPermission
 
+
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    queryset = Product.objects.prefetch_related('images').all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filter_class = ProductFilter

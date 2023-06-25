ae209f42caac31d3f0a82094cc90b72b9579433b
Dylan O'Brien <dylanobrien1985@gmail.com>
2023-06-25 01:43:01 +0100

    Implementing basic email functions to app

--- storefront/settings.py
+++ storefront/settings.py
@@ -170,3 +170,15 @@ DJOSER = {
         'current_user': 'core.serializers.UserSerializer'
     }
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = 'from@dylanbuy.com'
+
+ADMINS = [
+    ('Dylan', 'admin@dylanbuy.com')
+]
